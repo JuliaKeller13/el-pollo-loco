f34e5a40ebdc6d0e84ff1fbbf0532ebb8e8b6cf8
Julia Keller <juliakeller.vk@gmail.com>
2026-02-21 00:46:47 +0100

    level rename

---
+++ levels/level1.js
@@ -0,0 +1,5 @@
+const level1 = new Level(
+    [new Chicken(), new Chicken(), new Chicken()],
+    [new Cloud(), new Cloud()],
+    drawBackground()
+);
--- models/level.class.js
+++ models/level.class.js
@@ -10,3 +10,17 @@ class Level {
         this.backgroundObjects = backgroundObjects;
     }
 }
+
+function drawBackground() {
+    let backgroundObjects = [];
+    for (let i = -1; i < 5; i++) {
+      let posX = i * 719;
+      let imageNum = (i % 2 === 0) ? 1 : 2;
+      backgroundObjects.push(
+        new BackgroundObject(`assets/img/5_background/layers/3_third_layer/${imageNum}.png`, posX),
+        new BackgroundObject(`assets/img/5_background/layers/2_second_layer/${imageNum}.png`, posX),
+        new BackgroundObject(`assets/img/5_background/layers/1_first_layer/${imageNum}.png`, posX),
+      );
+  }
+  return backgroundObjects;
+}
