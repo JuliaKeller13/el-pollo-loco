b3eeba95bde6b0d033730236c3c6bc4a22424106
Julia Keller <juliakeller.vk@gmail.com>
2026-02-20 12:58:05 +0100

    background added

--- models/world.class.js
+++ models/world.class.js
@@ -2,11 +2,7 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud(), new Cloud()];
-  backgroundObjects = [
-    new BackgroundObject("assets/img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("assets/img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("assets/img/5_background/layers/1_first_layer/1.png", 0),
-  ];
+  backgroundObjects = [];
   canvas;
   ctx;
   keyboard;
@@ -18,7 +14,20 @@ class World {
     this.keyboard = keyboard;
     this.draw();
     this.setWorld();
+    this.drawBackground();
   }
+
+  drawBackground() {
+    for (let i = -2; i < 4; i++) {
+      let posX = i * 720;
+      let imageNum = (Math.abs(i) % 2 === 0) ? 1 : 2;
+      this.backgroundObjects.push(
+        new BackgroundObject(`assets/img/5_background/layers/3_third_layer/${imageNum}.png`, posX),
+        new BackgroundObject(`assets/img/5_background/layers/2_second_layer/${imageNum}.png`, posX),
+        new BackgroundObject(`assets/img/5_background/layers/1_first_layer/${imageNum}.png`, posX),
+      );
+  }
+}
 
   setWorld() {
     this.character.world = this;
@@ -36,7 +45,6 @@ class World {
 
     this.ctx.translate(-this.cameraX, 0);
 
-
     let self = this;
     requestAnimationFrame(function () {
       self.draw();
@@ -50,14 +58,14 @@ class World {
   }
 
   addToMap(mo) {
-    if (mo.otherDirection){
+    if (mo.otherDirection) {
       this.ctx.save();
       this.ctx.translate(mo.width, 0);
       this.ctx.scale(-1, 1);
       mo.posX = mo.posX / -1;
     }
     this.ctx.drawImage(mo.img, mo.posX, mo.posY, mo.width, mo.height);
-    if (mo.otherDirection){
+    if (mo.otherDirection) {
       mo.posX = mo.posX / -1;
       this.ctx.restore();
     }
